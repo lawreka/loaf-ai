043af2c3252b9f6ce5ea5eb21ae679d145efe079
Kathryn Lawrence <lawr3ka@gmail.com>
2020-08-16 05:55:52 +0200

    start makin som beats

---
+++ chords.js
@@ -0,0 +1,31 @@
+const thatRainyDay = [
+    ['0:0:0', 'G1']
+    ['0:0:0', 'D2'], 
+    ['0:0:0', 'F#2'], 
+    ['0:0:0', 'B2'], 
+    ['0:0:0', 'D3'],
+    ['0:2:0', 'E2'],
+    ['0:2:0', 'G#2'],
+    ['0:2:0', 'A#2'],
+    ['0:2:0', 'D3'],
+    ['0:2:0', 'A#3'],
+    ['1:0:0', 'A1'],
+    ['1:0:0', 'E2'],
+    ['1:0:0', 'G2'],
+    ['1:0:0', 'C3'],
+    ['1:0:0', 'D3'],
+    ['1:2:0', 'D2'],
+    ['1:2:0', 'F#2'],
+    ['1:2:0', 'C3'],
+    ['1:2:0', 'D#3'],
+    ['1:2:0', 'F#3'],
+    ['1:2:0', 'C4'],
+]
+
+const samples = {
+    'A0': 'https://github.com/nbrosowsky/tonejs-instruments/tree/master/samples/piano/A0.mp3',
+    'A1': 'https://github.com/nbrosowsky/tonejs-instruments/tree/master/samples/piano/A1.mp3',
+    'A2': 'https://github.com/nbrosowsky/tonejs-instruments/tree/master/samples/piano/A2.mp3',
+    'A3': 'https://github.com/nbrosowsky/tonejs-instruments/tree/master/samples/piano/A3.mp3',
+    'A4': 'https://github.com/nbrosowsky/tonejs-instruments/tree/master/samples/piano/A4.mp3'
+}
